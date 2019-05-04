acf83bf68591860885983429a66a51795718476a
Hongzi Mao <hongzi@mit.edu>
2019-05-04 19:35:37 -0400

    update script

--- script.py
+++ script.py
@@ -24,7 +24,7 @@ prefix = '10_value_networks'
 log_file = open(log_path + prefix, 'w')
 log_files.append(log_file)
 p = subprocess.Popen(
-    'python3 load_balance_actor_critic_train.py ' + \
+    'python3 load_balance_actor_multi_critic_train.py ' + \
     '--num_workers 10 --service_rates 0.15 0.25 0.35 0.45 0.55 0.65 0.75 0.85 0.95 1.05 '
     '--result_folder ./results/' + prefix + '/ ' + \
     '--model_folder ./results/parameters/' + prefix + '/',
