4b2ff40325d872683a97a0cd571d74dc86c9c539
Hongzi Mao <hongzi@mit.edu>
2019-05-05 11:00:20 -0400

    rename

--- tensorboard_summaries.py
+++ tensorboard_summaries.py
@@ -25,8 +25,8 @@ def build_load_balance_tf_summaries():
     tf.summary.scalar("Episode length", eps_length)
     average_reward = tf.Variable(0.)
     tf.summary.scalar("Average number of concurrent jobs", average_reward)
-    sum_diff_reward = tf.Variable(0.)
-    tf.summary.scalar("Sum differential reward", sum_diff_reward)
+    sum_rewards = tf.Variable(0.)
+    tf.summary.scalar("Sum of rewards", sum_rewards)
     eps_duration = tf.Variable(0.)
     tf.summary.scalar("Episode duration in seconds", eps_duration)
     entropy_weight = tf.Variable(0.)
@@ -49,7 +49,7 @@ def build_load_balance_tf_summaries():
     tf.summary.scalar("Average job duration", average_job_duration)
 
     summary_vars = [action_loss, entropy, value_loss, eps_length, \
-                    average_reward, sum_diff_reward, eps_duration, \
+                    average_reward, sum_rewards, eps_duration, \
                     entropy_weight, reset_prob, num_stream_jobs, \
                     reset_hit, eps_finished_jobs, eps_unfinished_jobs, \
                     eps_finished_work, eps_unfinished_work, average_job_duration]
